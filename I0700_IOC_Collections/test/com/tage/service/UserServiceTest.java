18540d061bcb13bef8450c091ddc8a7864c5ef82
tage <763409282@qq.com>
2016-03-27 23:29:57 +0800

    IOC_Collections

---
+++ I0700_IOC_Collections/test/com/tage/service/UserServiceTest.java
@@ -0,0 +1,42 @@
+package com.tage.service;
+
+
+import com.tage.dao.UserDAO;
+import com.tage.model.User;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by tage on 3/26/16.
+ */
+public class UserServiceTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testAdd() throws Exception {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");//ApplicationContext 实现 BeanFactory接口
+
+
+
+
+        UserDAO userDAO= (UserDAO) applicationContext.getBean("userDAO");
+        System.out.println(userDAO);
+
+
+    }
+}
